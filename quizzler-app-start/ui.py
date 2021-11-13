511d5f778a4daf8455a330271e4b741482e67f0d
jinicha <jinichaa@gmail.com>
2021-11-12 21:22:28 -0800

    Create class QuizUI

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -1,2 +1,29 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        # create a label
+        self.score = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score.grid(row=0, column=1)
+
+        # create canvas
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.question = self.canvas.create_text(150, 125, text="question goes here", width=250, font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        # create two buttons
+        true_img = PhotoImage(file="images/true.png")
+        false_img = PhotoImage(file="images/false.png")
+        self.true_btn = Button(image=true_img, highlightthickness=0)
+        self.false_btn = Button(image=false_img, highlightthickness=0)
+        self.true_btn.grid(row=2, column=0)
+        self.false_btn.grid(row=2, column=1)
+
+        self.window.mainloop()
